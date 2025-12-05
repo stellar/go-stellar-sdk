f4ec2b9b6f19a9a502c91229917e3a4ff759f75d
Christian Jonas <christianjonas99@gmail.com>
2025-12-05 01:01:58 -0500

    added struct for Soroban network limits

--- protocols/stellarcore/info_response.go
+++ protocols/stellarcore/info_response.go
@@ -27,6 +27,77 @@ type LedgerInfo struct {
 	Version      int    `json:"version"`
 }
 
+type SorobanInfoResponse struct {
+	// Contract settings
+	MaxContractSize          int `json:"max_contract_size"`
+	MaxContractDataKeySize   int `json:"max_contract_data_key_size"`
+	MaxContractDataEntrySize int `json:"max_contract_data_entry_size"`
+
+	// Compute settings
+	Tx struct {
+		MaxInstructions            int64  `json:"max_instructions"`
+		MemoryLimit                uint32 `json:"memory_limit"`
+		MaxReadLedgerEntries       uint32 `json:"max_read_ledger_entries"`
+		MaxReadBytes               uint32 `json:"max_read_bytes"`
+		MaxWriteLedgerEntries      uint32 `json:"max_write_ledger_entries"`
+		MaxWriteBytes              uint32 `json:"max_write_bytes"`
+		MaxFootprintSize           uint32 `json:"max_footprint_size,omitempty"`
+		MaxContractEventsSizeBytes uint32 `json:"max_contract_events_size_bytes"`
+		MaxSizeBytes               uint32 `json:"max_size_bytes"`
+	} `json:"tx"`
+
+	Ledger struct {
+		MaxInstructions       int64  `json:"max_instructions"`
+		MaxReadLedgerEntries  uint32 `json:"max_read_ledger_entries"`
+		MaxReadBytes          uint32 `json:"max_read_bytes"`
+		MaxWriteLedgerEntries uint32 `json:"max_write_ledger_entries"`
+		MaxWriteBytes         uint32 `json:"max_write_bytes"`
+		MaxTxSizeBytes        uint32 `json:"max_tx_size_bytes"`
+		MaxTxCount            uint32 `json:"max_tx_count"`
+	} `json:"ledger"`
+
+	// FeeRatePerInstructionsIncrement int64 `json:"fee_rate_per_instructions_increment"`
+
+	// // Fees
+	// FeeReadLedgerEntry      int64 `json:"fee_read_ledger_entry"`
+	// FeeWriteLedgerEntry     int64 `json:"fee_write_ledger_entry"`
+	// FeeRead1KB              int64 `json:"fee_read_1kb"`
+	// FeeWrite1KB             int64 `json:"fee_write_1kb"`
+	// FeeHistorical1KB        int64 `json:"fee_historical_1kb"`
+	// FeeContractEventsSize1KB int64 `json:"fee_contract_events_size_1kb"`
+	// FeeTransactionSize1KB   int64 `json:"fee_transaction_size_1kb"`
+
+	// // State archival settings
+	// StateArchival struct {
+	//     MaxEntryTTL                         uint32 `json:"max_entry_ttl"`
+	//     MinTemporaryTTL                     uint32 `json:"min_temporary_ttl"`
+	//     MinPersistentTTL                    uint32 `json:"min_persistent_ttl"`
+	//     PersistentRentRateDenominator       int64  `json:"persistent_rent_rate_denominator"`
+	//     TempRentRateDenominator             int64  `json:"temp_rent_rate_denominator"`
+	//     MaxEntriesToArchive                 uint32 `json:"max_entries_to_archive"`
+	//     BucketListSizeWindowSampleSize      uint32 `json:"bucketlist_size_window_sample_size"`
+	//     EvictionScanSize                    uint64 `json:"eviction_scan_size"`
+	//     StartingEvictionScanLevel           uint32 `json:"starting_eviction_scan_level"`
+	//     BucketListSizeSnapshotPeriod        uint32 `json:"bucket_list_size_snapshot_period"`
+	//     AverageBucketListSize               uint64 `json:"average_bucket_list_size"` // Non-configurable
+	// } `json:"state_archival"`
+
+	// // Protocol v23+ only
+	// MaxDependentTxClusters uint32 `json:"max_dependent_tx_clusters,omitempty"`
+
+	// // SCP timing settings (Protocol v23+ only)
+	// SCP *SCPSettings `json:"scp,omitempty"`
+}
+
+// // SCPSettings represents SCP timing configuration (Protocol v23+)
+// type SCPSettings struct {
+//     LedgerCloseTimeMS       uint32 `json:"ledger_close_time_ms"`
+//     NominationTimeoutMS     uint32 `json:"nomination_timeout_ms"`
+//     NominationTimeoutIncMS  uint32 `json:"nomination_timeout_inc_ms"`
+//     BallotTimeoutMS         uint32 `json:"ballot_timeout_ms"`
+//     BallotTimeoutIncMS      uint32 `json:"ballot_timeout_inc_ms"`
+// }
+
 // IsSynced returns a boolean indicating whether stellarcore is synced with the
 // network.
 func (resp *InfoResponse) IsSynced() bool {

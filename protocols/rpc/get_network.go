03a14cee3f20da53fff8514fb13d115890e0e70c
Christian Jonas <christianjonas99@gmail.com>
2025-12-05 13:43:32 -0500

    added nested network limits struct to GetNetworkResponse struct

--- protocols/rpc/get_network.go
+++ protocols/rpc/get_network.go
@@ -4,9 +4,21 @@ const GetNetworkMethodName = "getNetwork"
 
 type GetNetworkRequest struct{}
 
+type GetNetworkLimitsResponse struct {
+	MaxContractSize          int    `json:"maxContractSize"`
+	MaxContractDataKeySize   int    `json:"maxContractDataKeySize"`
+	MaxContractDataEntrySize int    `json:"maxContractDataEntrySize"`
+	MaxTxInstructions        int64  `json:"maxTxInstructions"`
+	MaxTxReadLedgerEntries   uint32 `json:"maxTxReadLedgerEntries"`
+	MaxTxReadBytes           uint32 `json:"maxTxReadBytes"`
+	MaxTxWriteLedgerEntries  uint32 `json:"maxTxWriteLedgerEntries"`
+	MaxTxWriteBytes          uint32 `json:"maxTxWriteBytes"`
+}
+
 type GetNetworkResponse struct {
-	FriendbotURL    string `json:"friendbotUrl,omitempty"`
-	Passphrase      string `json:"passphrase"`
-	ProtocolVersion int    `json:"protocolVersion"`
-	Build           string `json:"build"`
+	FriendbotURL    string                   `json:"friendbotUrl,omitempty"`
+	Passphrase      string                   `json:"passphrase"`
+	ProtocolVersion int                      `json:"protocolVersion"`
+	Build           string                   `json:"build"`
+	Limits          GetNetworkLimitsResponse `json:"limits"`
 }

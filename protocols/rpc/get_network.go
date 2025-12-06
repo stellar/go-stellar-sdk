5afc28f7fb0a72a1944a98f0b1d0c87408a40321
Christian Jonas <christianjonas99@gmail.com>
2025-12-05 19:27:13 -0500

    revisited included network limits; reorganized returned struct

--- protocols/rpc/get_network.go
+++ protocols/rpc/get_network.go
@@ -1,19 +1,12 @@
 package protocol
 
+import (
+	"github.com/stellar/go-stellar-sdk/protocols/stellarcore"
+)
+
 const GetNetworkMethodName = "getNetwork"
 
 type GetNetworkRequest struct{}
-
-type GetNetworkLimitsResponse struct {
-	MaxContractSize          int    `json:"maxContractSize"`
-	MaxContractDataKeySize   int    `json:"maxContractDataKeySize"`
-	MaxContractDataEntrySize int    `json:"maxContractDataEntrySize"`
-	MaxTxInstructions        int64  `json:"maxTxInstructions"`
-	MaxTxReadLedgerEntries   uint32 `json:"maxTxReadLedgerEntries"`
-	MaxTxReadBytes           uint32 `json:"maxTxReadBytes"`
-	MaxTxWriteLedgerEntries  uint32 `json:"maxTxWriteLedgerEntries"`
-	MaxTxWriteBytes          uint32 `json:"maxTxWriteBytes"`
-}
 
 type GetProtocolVersions struct {
 	MinSupportedProtocolVersion  int `json:"minSupportedProtocolVersion"`
@@ -22,9 +15,9 @@ type GetProtocolVersions struct {
 }
 
 type GetNetworkResponse struct {
-	FriendbotURL     string                   `json:"friendbotUrl,omitempty"`
-	Passphrase       string                   `json:"passphrase"`
-	Build            string                   `json:"build"`
-	ProtocolVersions GetProtocolVersions      `json:"protocolVersions"`
-	Limits           GetNetworkLimitsResponse `json:"limits"`
+	FriendbotURL     string                          `json:"friendbotUrl,omitempty"`
+	Passphrase       string                          `json:"passphrase"`
+	Build            string                          `json:"build"`
+	ProtocolVersions GetProtocolVersions             `json:"protocolVersions"`
+	Limits           stellarcore.SorobanInfoResponse `json:"limits"`
 }
--- protocols/stellarcore/info_response.go
+++ protocols/stellarcore/info_response.go
@@ -9,8 +9,6 @@ type InfoResponse struct {
 		ProtocolVersion int        `json:"protocol_version"`
 		State           string     `json:"state"`
 		Ledger          LedgerInfo `json:"ledger"`
-
-		// TODO: all the other fields
 	}
 }
 
@@ -33,7 +31,7 @@ type SorobanInfoResponse struct {
 	MaxContractDataKeySize   int `json:"max_contract_data_key_size"`
 	MaxContractDataEntrySize int `json:"max_contract_data_entry_size"`
 
-	// Compute settings
+	// Compute settings/per-transaction limits
 	Tx struct {
 		MaxInstructions            int64  `json:"max_instructions"`
 		MemoryLimit                uint32 `json:"memory_limit"`
@@ -41,11 +39,12 @@ type SorobanInfoResponse struct {
 		MaxReadBytes               uint32 `json:"max_read_bytes"`
 		MaxWriteLedgerEntries      uint32 `json:"max_write_ledger_entries"`
 		MaxWriteBytes              uint32 `json:"max_write_bytes"`
-		MaxFootprintSize           uint32 `json:"max_footprint_size,omitempty"`
+		MaxFootprintSize           uint32 `json:"max_footprint_size,omitempty"` // protocol v23+
 		MaxContractEventsSizeBytes uint32 `json:"max_contract_events_size_bytes"`
 		MaxSizeBytes               uint32 `json:"max_size_bytes"`
 	} `json:"tx"`
 
+	// Ledger-wide limits
 	Ledger struct {
 		MaxInstructions       int64  `json:"max_instructions"`
 		MaxReadLedgerEntries  uint32 `json:"max_read_ledger_entries"`
@@ -56,50 +55,45 @@ type SorobanInfoResponse struct {
 		MaxTxCount            uint32 `json:"max_tx_count"`
 	} `json:"ledger"`
 
-	// FeeRatePerInstructionsIncrement int64 `json:"fee_rate_per_instructions_increment"`
+	FeeRatePerInstructionsIncrement int64 `json:"fee_rate_per_instructions_increment"`
 
-	// // Fees
-	// FeeReadLedgerEntry      int64 `json:"fee_read_ledger_entry"`
-	// FeeWriteLedgerEntry     int64 `json:"fee_write_ledger_entry"`
-	// FeeRead1KB              int64 `json:"fee_read_1kb"`
-	// FeeWrite1KB             int64 `json:"fee_write_1kb"`
-	// FeeHistorical1KB        int64 `json:"fee_historical_1kb"`
-	// FeeContractEventsSize1KB int64 `json:"fee_contract_events_size_1kb"`
-	// FeeTransactionSize1KB   int64 `json:"fee_transaction_size_1kb"`
+	// Fees
+	FeeReadLedgerEntry       int64 `json:"fee_read_ledger_entry"`
+	FeeWriteLedgerEntry      int64 `json:"fee_write_ledger_entry"`
+	FeeRead1KB               int64 `json:"fee_read_1kb"`
+	FeeWrite1KB              int64 `json:"fee_write_1kb"`
+	FeeHistorical1KB         int64 `json:"fee_historical_1kb"`
+	FeeContractEventsSize1KB int64 `json:"fee_contract_events_size_1kb"`
+	FeeTransactionSize1KB    int64 `json:"fee_transaction_size_1kb"`
 
-	// // State archival settings
-	// StateArchival struct {
-	//     MaxEntryTTL                         uint32 `json:"max_entry_ttl"`
-	//     MinTemporaryTTL                     uint32 `json:"min_temporary_ttl"`
-	//     MinPersistentTTL                    uint32 `json:"min_persistent_ttl"`
-	//     PersistentRentRateDenominator       int64  `json:"persistent_rent_rate_denominator"`
-	//     TempRentRateDenominator             int64  `json:"temp_rent_rate_denominator"`
-	//     MaxEntriesToArchive                 uint32 `json:"max_entries_to_archive"`
-	//     BucketListSizeWindowSampleSize      uint32 `json:"bucketlist_size_window_sample_size"`
-	//     EvictionScanSize                    uint64 `json:"eviction_scan_size"`
-	//     StartingEvictionScanLevel           uint32 `json:"starting_eviction_scan_level"`
-	//     BucketListSizeSnapshotPeriod        uint32 `json:"bucket_list_size_snapshot_period"`
-	//     AverageBucketListSize               uint64 `json:"average_bucket_list_size"` // Non-configurable
-	// } `json:"state_archival"`
+	// State archival settings
+	StateArchival struct {
+		MaxEntryTTL                    uint32 `json:"max_entry_ttl"`
+		MinTemporaryTTL                uint32 `json:"min_temporary_ttl"`
+		MinPersistentTTL               uint32 `json:"min_persistent_ttl"`
+		PersistentRentRateDenominator  int64  `json:"persistent_rent_rate_denominator"`
+		TempRentRateDenominator        int64  `json:"temp_rent_rate_denominator"`
+		MaxEntriesToArchive            uint32 `json:"max_entries_to_archive"`
+		BucketListSizeWindowSampleSize uint32 `json:"bucketlist_size_window_sample_size"`
+		EvictionScanSize               uint64 `json:"eviction_scan_size"`
+		StartingEvictionScanLevel      uint32 `json:"starting_eviction_scan_level"`
+		BucketListSizeSnapshotPeriod   uint32 `json:"bucket_list_size_snapshot_period"`
+		AverageBucketListSize          uint64 `json:"average_bucket_list_size"` // Non-configurable
+	} `json:"state_archival"`
 
-	// // Protocol v23+ only
-	// MaxDependentTxClusters uint32 `json:"max_dependent_tx_clusters,omitempty"`
+	MaxDependentTxClusters uint32 `json:"max_dependent_tx_clusters"` // protocol v23+
 
-	// // SCP timing settings (Protocol v23+ only)
-	// SCP *SCPSettings `json:"scp,omitempty"`
+	// SCP timing config settings
+	SCPSettings struct {
+		LedgerCloseTimeMS      uint32 `json:"ledger_close_time_ms"`
+		NominationTimeoutMS    uint32 `json:"nomination_timeout_ms"`
+		NominationTimeoutIncMS uint32 `json:"nomination_timeout_inc_ms"`
+		BallotTimeoutMS        uint32 `json:"ballot_timeout_ms"`
+		BallotTimeoutIncMS     uint32 `json:"ballot_timeout_inc_ms"`
+	} `json:"scp"`
 }
 
-// // SCPSettings represents SCP timing configuration (Protocol v23+)
-// type SCPSettings struct {
-//     LedgerCloseTimeMS       uint32 `json:"ledger_close_time_ms"`
-//     NominationTimeoutMS     uint32 `json:"nomination_timeout_ms"`
-//     NominationTimeoutIncMS  uint32 `json:"nomination_timeout_inc_ms"`
-//     BallotTimeoutMS         uint32 `json:"ballot_timeout_ms"`
-//     BallotTimeoutIncMS      uint32 `json:"ballot_timeout_inc_ms"`
-// }
-
-// IsSynced returns a boolean indicating whether stellarcore is synced with the
-// network.
+// bool indicating whether stellarcore is synced with the network.
 func (resp *InfoResponse) IsSynced() bool {
 	return resp.Info.State == "Synced!"
 }

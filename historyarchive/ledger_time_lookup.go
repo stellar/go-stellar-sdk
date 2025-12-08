d4545d0b8d40c5abf6ca2240459af475e2958654
urvisavla <urvi.savla@stellar.org>
2025-12-08 09:56:03 -0800

    Update historyarchive/ledger_time_lookup.go
    Co-authored-by: tamirms <tamirms@gmail.com>

--- historyarchive/ledger_time_lookup.go
+++ historyarchive/ledger_time_lookup.go
@@ -13,7 +13,7 @@ const firstLedgerWithRealTime int64 = 2 // ledger 1 has closeTime = 0 (Unix epoc
 // LedgerForTime returns the smallest ledger sequence L such that
 // closeTime(L) >= target. If target is before the first ledger, it
 // returns the genesis ledger. If after the latest, it returns the
-// latest ledger.
+// latest checkpoint ledger.
 func LedgerForTime(archive ArchiveInterface, target time.Time) (int64, error) {
 	root, err := archive.GetRootHAS()
 	if err != nil {

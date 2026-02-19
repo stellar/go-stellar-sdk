66dc93b51a4e823b98f7e20f6cc6af3eb2e18d4a
Christian Jonas <christianjonas99@gmail.com>
2026-02-19 16:45:48 -0500

    added 'Try*' functions to give safe alternatives to price utils, reverted old price utils

--- xdr/price.go
+++ xdr/price.go
@@ -1,7 +1,6 @@
 package xdr
 
 import (
-	"fmt"
 	"math/big"
 
 	"github.com/stellar/go-stellar-sdk/support/errors"
@@ -9,27 +8,38 @@ import (
 
 // String returns a string representation of `p`
 func (p Price) String() string {
-	if err := p.Validate(); err != nil {
-		fmt.Printf("cannot stringify invalid price: %v\n", err)
-		return "INVALID"
-	}
 	return big.NewRat(int64(p.N), int64(p.D)).FloatString(7)
+}
+
+// TryString returns a string representation of `p`.
+// Returns an error if `p` is invalid.
+func (p Price) TryString() (string, error) {
+	if err := p.Validate(); err != nil {
+		return "", errors.Wrap(err, "invalid price")
+	}
+	return p.String(), nil
 }
 
 // Equal returns whether the price's value is the same,
 // taking into account denormalized representation
 // (e.g. Price{1, 2}.EqualValue(Price{2,4}) == true )
 func (p Price) Equal(q Price) bool {
-	if err := p.Validate(); err != nil {
-		fmt.Printf("cannot compare invalid price: %v\n", err)
-		return false
-	}
-	if err := q.Validate(); err != nil {
-		fmt.Printf("cannot compare invalid price: %v\n", err)
-		return false
-	}
 	// See the Cheaper() method for the reasoning behind this:
 	return uint64(p.N)*uint64(q.D) == uint64(q.N)*uint64(p.D)
+}
+
+// TryEqual returns whether the price's value is the same,
+// taking into account denormalized representation
+// (e.g. Price{1, 2}.EqualValue(Price{2,4}) == true )
+// Returns an error if either price is invalid.
+func (p Price) TryEqual(q Price) (bool, error) {
+	if err := p.Validate(); err != nil {
+		return false, errors.Wrap(err, "invalid price p")
+	}
+	if err := q.Validate(); err != nil {
+		return false, errors.Wrap(err, "invalid price q")
+	}
+	return p.Equal(q), nil
 }
 
 // Cheaper indicates if the Price's value is lower,
@@ -46,35 +56,56 @@ func (p Price) Cheaper(q Price) bool {
 	// (p.N / p.D) * (p.D * q.D) < (q.N / q.D) * (p.D * q.D)
 	//  <==>
 	// p.N * q.D < q.N * p.D
+	return uint64(p.N)*uint64(q.D) < uint64(q.N)*uint64(p.D)
+}
+
+// TryCheaper indicated if the Price's value is lower,
+// taking into account denormalized representation.
+// (e.g. Price{1, 2}.Cheaper(Price{2,4}) == false )
+// Returns an error if either price is invalid
+func (p Price) TryCheaper(q Price) (bool, error) {
 	if err := p.Validate(); err != nil {
-		fmt.Printf("cannot compare invalid price: %v\n", err)
-		return false
+		return false, errors.Wrap(err, "invalid price p")
 	}
 	if err := q.Validate(); err != nil {
-		fmt.Printf("cannot compare invalid price: %v\n", err)
-		return false
+		return false, errors.Wrap(err, "invalid price q")
 	}
-	return uint64(p.N)*uint64(q.D) < uint64(q.N)*uint64(p.D)
+	return p.Cheaper(q), nil
 }
 
 // Normalize sets Price to its rational canonical form
 func (p *Price) Normalize() {
-	if err := p.Validate(); err != nil {
-		fmt.Printf("cannot normalize invalid price: %v\n", err)
-		return
-	}
 	r := big.NewRat(int64(p.N), int64(p.D))
 	p.N = Int32(r.Num().Int64())
 	p.D = Int32(r.Denom().Int64())
 }
 
+// TryNormalize sets the price to its rational canonical form.
+// Returns an error if the price is invalid
+func (p *Price) TryNormalize() error {
+	if err := p.Validate(); err != nil {
+		return errors.Wrap(err, "invalid price")
+	}
+	p.Normalize()
+	return nil
+}
+
 // Invert inverts Price.
 func (p *Price) Invert() {
 	if err := p.Validate(); err != nil {
-		fmt.Printf("cannot invert invalid price: %v\n", err)
 		return
 	}
 	p.N, p.D = p.D, p.N
+}
+
+// TryInvert inverts Price.
+// Returns an error if the price is invalid
+func (p *Price) TryInvert() error {
+	if err := p.Validate(); err != nil {
+		return errors.Wrap(err, "invalid price")
+	}
+	p.Invert()
+	return nil
 }
 
 // Validate checks if the price is valid and returns an error if not.
